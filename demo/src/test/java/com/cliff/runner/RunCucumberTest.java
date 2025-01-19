d512e3b5df557dd467bc29c4db31dddfde7a4d87
cliff_wang <codingcliff@pm.me>
2025-01-19 01:12:47 -0500

    fix the testng setup to support allure report

--- demo/src/test/java/com/cliff/runner/RunCucumberTest.java
+++ demo/src/test/java/com/cliff/runner/RunCucumberTest.java
@@ -4,7 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features",
-        plugin = {"pretty", "html:target/cucumber-html-report"},// Path to feature files
+        plugin = {"pretty", "html:target/cucumber-html-report",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         glue = {"com.cliff.hooks", "com.cliff.steps"})          // Package with step definitions
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 }
